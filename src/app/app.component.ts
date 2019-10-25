6d26a8f5494d0997cf1c89e12bd18ddf1e77582a
irek02 <imirgaleev@gmail.com>
2019-10-24 19:46:07 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { FormBuilder } from '@angular/forms';
 export class AppComponent {
 
   signUpForm = this.fb.group({
-    email: ['']
+    email: ['', Validators.email]
   });
 
   constructor(private fb: FormBuilder) { }
